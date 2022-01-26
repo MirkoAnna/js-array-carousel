27e5fc362985bae7f3fb1418b57b6a0f71b45048
Mirko <mirkoanna137@gmail.com>
2022-01-26 19:27:24 +0100

    milestone 2

--- main.js
+++ main.js
@@ -22,14 +22,25 @@ const description = [
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
 ];
 
+let mainImage = '';
 let listImages = '';
 
 for (let i = 0; i < images.length; i++ ) {
     listImages += `<div class="image">
                     <img src="${images[i]}" alt="">
                 </div>`;
+                
+    mainImage = `<div class="main-image">
+                    <img src="${images[i]}" alt="">
+                    <div class="text">
+                        <h2 id="country">${nameCountries[i]}</h2>
+                        <h4 id="description">${description[i]}</h4>
+                    </div>
+                <div>`;
 }
 
-let printerListImages = document.querySelector('.container-list-images');
-printerListImages.innerHTML = listImages;
+let listImagesInjector  = document.querySelector('.container-list-images');
+listImagesInjector.innerHTML = listImages;
 
+let mainImageInjector = document.querySelector('.container-main-image');
+mainImageInjector.innerHTML = mainImage;
